aeb514078ffc06d87e503b9b86fe21b53a581585
ouattara <ouattaraibrahimyacouba@gmail.com>
2024-08-13 21:27:14 +0000

    definition des routes de base

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 // import React from 'react'
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import UserLayout from "./layout/UserLayout"
+import Home from "./routes/Home"
+import Projects from "./routes/Projects"
+import Portfolio from "./routes/Portfolio"
+import Blog from "./routes/Blog"
+import Contact from "./routes/Contact"
+
 const App = () => {
+  // creation des routes
+  const router = createBrowserRouter([
+    {
+      path: '/', element: <UserLayout />, children: [
+        { path: '', element: <Home /> },
+        { path: 'projects', element: <Projects /> },
+        { path: 'porfolio', element: <Portfolio /> },
+        { path: 'blog', element: <Blog /> },
+        { path: 'contact', element: <Contact /> },
+      ]
+    }
+  ])
   return (
-    <div>App</div>
+    <RouterProvider router={router} />
   )
 }
 
