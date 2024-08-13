7c46166efdcca95f5f1c3f06715df7d6cb0fefe6
ouattara <ouattaraibrahimyacouba@gmail.com>
2024-08-13 21:09:45 +0000

    reference des outils de firebase

--- src/firebaseConfig/firebase.js
+++ src/firebaseConfig/firebase.js
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +18,15 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Avoir la référence de notre base de données
+export const db = getFirestore(app)
+
+// Avoir la référence du service d'authentification
+export const auth = getAuth(app)
+
+// Avoir la référence du storage pour stockage des images
+export const storage = getStorage(app)
+
+// Avoir la référence du service d'authentification avec Google
+export const googleProvider = new GoogleAuthProvider()
