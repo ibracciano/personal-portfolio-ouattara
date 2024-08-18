dbd41bac1f851c37b504e41b1ebcece545c77266
ouattara <ouattaraibrahimyacouba@gmail.com>
2024-08-18 23:52:57 +0000

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import Projects from "./routes/Projects";
 import Portfolio from "./routes/Portfolio";
 import Blog from "./routes/Blog";
 import Contact from "./routes/Contact";
+import Dashboard from "./routes/admin/Dashboard";
+import AdminLayout from "./layout/AdminLayout";
+import AllPosts from "./routes/admin/AllPosts";
+import AddPost from "./routes/admin/AddPost";
 
 const App = () => {
   // creation des routes
@@ -20,6 +24,15 @@ const App = () => {
         { path: "resume", element: <Portfolio /> },
         { path: "blog", element: <Blog /> },
         { path: "contact", element: <Contact /> },
+        {
+          path: "dashboard",
+          element: <AdminLayout />,
+          children: [
+            { path: "", element: <Dashboard /> },
+            { path: "all-post", element: <AllPosts /> },
+            { path: "add-post", element: <AddPost /> },
+          ],
+        },
       ],
     },
   ]);
--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,9 +1,58 @@
 // import React from 'react'
 
-const AdminLayout = () => {
-    return (
-        <div>AdminLayout</div>
-    )
-}
+import { FaBorderAll } from "react-icons/fa";
+import { MdAddCircle } from "react-icons/md";
+import { NavLink, Outlet } from "react-router-dom";
 
-export default AdminLayout
+const AdminLayout = () => {
+  return (
+    <main className="font-body">
+      <div className="flex">
+        {/* Menu de navigation */}
+        <nav className="w-[22%] py-2 rounded-r-lg pr-2 my-[20%] bg-black">
+          <h1 className="mb-5 text-center text-transparent OrangeToPink bg-clip-text">
+            Dashboard
+          </h1>
+          <ul className="flex flex-col gap-5">
+            <li>
+              <NavLink
+                to="/dashboard/add-post"
+                className={({ isActive }) =>
+                  isActive
+                    ? "border-r-4 border-orange-500 px-2 py-1 bg-slate-800 flex items-center gap-2 rounded-r-full"
+                    : "px-2 flex items-center gap-2"
+                }
+              >
+                <MdAddCircle size={30} className="rounded-full OrangeToPink" />
+                Ajouter un post
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/dashboard/all-post"
+                className={({ isActive }) =>
+                  isActive
+                    ? "border-r-4 border-orange-500 px-2 py-1 bg-slate-800 flex items-center gap-2 rounded-r-full"
+                    : "px-2 flex items-center gap-2"
+                }
+              >
+                <FaBorderAll size={30} className=" OrangeToPink" />
+                Tous les posts
+              </NavLink>
+            </li>
+          </ul>
+          <button className="p-2 mt-5 font-bold text-transparent rounded-full OrangeToPink bg-clip-text">
+            Deconnexion
+          </button>
+        </nav>
+
+        {/* contenu de la page */}
+        <div>
+          <Outlet />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default AdminLayout;
